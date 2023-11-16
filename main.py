cc7841908a90db97e9624ec5c1d2e0ea3cbdcd6a
Levi <115052769+leviobrabo@users.noreply.github.com>
2023-11-16 00:25:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -235,6 +235,7 @@ def get_news(limit=5):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
         '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
+    news_list = []
     try:
         response = requests.get(url, timeout=10, headers=headers)
         if response.status_code != 200:
